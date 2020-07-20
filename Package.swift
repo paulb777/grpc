4551b2e46d97383a11aa8365840bd52b82a00f6e
Paul Beusterien <paulbeusterien@google.com>
2020-07-20 08:36:58 -0700

    Revert "combine targets"
    This reverts commit 31aa24ffd9bb1af1df9e652b47f14d9ec67a15de.

--- Package.swift
+++ Package.swift
@@ -11,30 +11,20 @@ let package = Package(
         .tvOS(.v9)
     ],
 
-  // products: [
-  //   .library(
-  //     name: "gRPC-Core",
-  //     targets: [
-  //       "gRPC-Core",
-  //     ]
-  //   ),
-  //   .library(
-  //     name: "gRPC-cpp",
-  //     targets: [
-  //       "gRPC-cpp",
-  //     ]
-  //   )
-  // ],
-
   products: [
     .library(
-      name: "gRPC",
+      name: "gRPC-Core",
       targets: [
-        "gRPC",
+        "gRPC-Core",
+      ]
+    ),
+    .library(
+      name: "gRPC-cpp",
+      targets: [
+        "gRPC-cpp",
       ]
     )
   ],
-
 
   dependencies: [
     .package(name: "abseil", url: "https://github.com/paulb777/abseil-cpp.git", .revision("6a901b3")),
@@ -43,7 +33,7 @@ let package = Package(
 
   targets: [
     .target(
-      name: "gRPC",
+      name: "gRPC-Core",
       dependencies: [
         .product(name:"abseil", package: "abseil"),
         .product(name:"openssl_grpc", package: "BoringSSL-GRPC"),
@@ -939,26 +929,26 @@ let package = Package(
         "third_party/upb/upb/table.int.h",
         "third_party/upb/upb/upb.c",
         "third_party/upb/upb/upb.h",
-  //     ],
-  //     publicHeadersPath: ".",
-  //     cSettings: [
-  //       .headerSearchPath("./"),
-  //       .headerSearchPath("include/"),
-  //       .headerSearchPath("third_party/upb/"),
-  //       .headerSearchPath("src/core/ext/upb-generated"),
-  // //      .headerSearchPath("../openssl_grpc/include"),
-  //       .define("GRPC_ARES", to: "0"),
-  //       .unsafeFlags(["-Wno-module-import-in-extern-c"]),
-  //     ]
-  //   ),
-  //   .target(
-  //     name: "gRPC-cpp",
-  //     dependencies: [
-  //       .product(name:"abseil", package: "abseil"),
-  //       "gRPC-Core",
-  //     ],
-  //     path: ".",
-  //     sources: [
+      ],
+      publicHeadersPath: ".",
+      cSettings: [
+        .headerSearchPath("./"),
+        .headerSearchPath("include/"),
+        .headerSearchPath("third_party/upb/"),
+        .headerSearchPath("src/core/ext/upb-generated"),
+  //      .headerSearchPath("../openssl_grpc/include"),
+        .define("GRPC_ARES", to: "0"),
+        .unsafeFlags(["-Wno-module-import-in-extern-c"]),
+      ]
+    ),
+    .target(
+      name: "gRPC-cpp",
+      dependencies: [
+        .product(name:"abseil", package: "abseil"),
+        "gRPC-Core",
+      ],
+      path: ".",
+      sources: [
         "include/grpcpp/alarm.h",
         "include/grpcpp/alarm_impl.h",
         "include/grpcpp/channel.h",
@@ -1544,7 +1534,6 @@ let package = Package(
         .headerSearchPath("third_party/upb/"),
         .headerSearchPath("src/core/ext/upb-generated"),
         .unsafeFlags(["-Wno-module-import-in-extern-c"]),
-        .define("GRPC_ARES", to: "0"),
       ]
     ),
   ],
